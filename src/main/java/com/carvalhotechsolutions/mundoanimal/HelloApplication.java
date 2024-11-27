c67f0c88bfabf5a5e7a6aa0f33dd912ae54f5bc1
SavioUFC <saviocarvalho@alu.ufc.br>
2024-11-27 09:37:27 -0300

    Commit Init

---
+++ src/main/java/com/carvalhotechsolutions/mundoanimal/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.carvalhotechsolutions.mundoanimal;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_teste.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("PetShop Mundo Animal");
+
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(screenBounds.getMinX());
+        stage.setY(screenBounds.getMinY());
+        stage.setWidth(screenBounds.getWidth());
+        stage.setHeight(screenBounds.getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.carvalhotechsolutions.mundoanimal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.carvalhotechsolutions.mundoanimal to javafx.fxml;
+    exports com.carvalhotechsolutions.mundoanimal;
+}
