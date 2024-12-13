27e47934e1bd7d68a7045a285f2349919a1e81af
Renan Soares <renanalencar313@gmail.com>
2024-12-13 10:20:30 -0300

    Finalização das entidades

--- src/main/java/com/carvalhotechsolutions/mundoanimal/model/Agendamento.java
+++ src/main/java/com/carvalhotechsolutions/mundoanimal/model/Agendamento.java
@@ -1,6 +1,121 @@
 package com.carvalhotechsolutions.mundoanimal.model;
 
+import com.carvalhotechsolutions.mundoanimal.model.enums.StatusAgendamento;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_agendamentos")
 public class Agendamento {
 
-    // Pendente - @BackEnd
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "data_agendamento", nullable = false)
+    private LocalDate dataAgendamento;
+
+    @Column(name = "horario_agendamento", nullable = false)
+    private LocalTime horarioAgendamento;
+
+    @ManyToOne
+    @JoinColumn(name = "servico_id", nullable = false)
+    private Servico servico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "animal_id", nullable = false)
+    private Animal animal;
+
+    @Column(name = "responsavel_atendimento")
+    private String responsavelAtendimento;
+
+    @Column(name = "horario_saida")
+    private LocalTime horarioSaida;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusAgendamento status;
+
+    public Agendamento() {
+        this.status = StatusAgendamento.PENDENTE;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDate dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+
+    public LocalTime getHorarioAgendamento() {
+        return horarioAgendamento;
+    }
+
+    public void setHorarioAgendamento(LocalTime horarioAgendamento) {
+        this.horarioAgendamento = horarioAgendamento;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public void setServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public String getResponsavelAtendimento() {
+        return responsavelAtendimento;
+    }
+
+    public void setResponsavelAtendimento(String responsavelAtendimento) {
+        this.responsavelAtendimento = responsavelAtendimento;
+    }
+
+    public LocalTime getHorarioSaida() {
+        return horarioSaida;
+    }
+
+    public void setHorarioSaida(LocalTime horarioSaida) {
+        this.horarioSaida = horarioSaida;
+    }
+
+    public StatusAgendamento getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusAgendamento status) {
+        this.status = status;
+    }
 }
