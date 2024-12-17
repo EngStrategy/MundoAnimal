175160e8666f706a7b8d89f5c86a539ed042fd85
Renan Soares <renanalencar313@gmail.com>
2024-12-17 19:27:08 -0300

    Creation of validation in recover password

--- src/main/java/com/carvalhotechsolutions/mundoanimal/controllers/login/RedefinirSenhaController.java
+++ src/main/java/com/carvalhotechsolutions/mundoanimal/controllers/login/RedefinirSenhaController.java
@@ -1,6 +1,7 @@
 package com.carvalhotechsolutions.mundoanimal.controllers.login;
 
 import com.carvalhotechsolutions.mundoanimal.utils.NavigationManager;
+import com.carvalhotechsolutions.mundoanimal.utils.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -10,6 +11,7 @@ public class RedefinirSenhaController {
     // Método temporário, apenas para testar a troca de telas, não há lógica alguma aplicada
     @FXML
     private void backToRecovery(ActionEvent event) throws IOException {
+        SessionManager.clearSession(); // Se ele desistir de redefinir a senha, deverá ser limpa a sessão
         NavigationManager.switchScene(event, "/fxml/autenticacao/recuperar-senha.fxml", "Recuperar senha");
     }
 }
--- src/main/java/com/carvalhotechsolutions/mundoanimal/utils/SessionManager.java
+++ src/main/java/com/carvalhotechsolutions/mundoanimal/utils/SessionManager.java
@@ -14,4 +14,8 @@ public class SessionManager {
         currentUser = user;
     }
 
+    public static void clearSession() {
+        currentUser = null;
+    }
+
 }
